ecc362b96bd5d673663ac1bebdce2c3b9952c563
SamheinDM <SamheinDM@yandex.ru>
2020-10-29 16:53:03 +0300

    feat: start extended-repeater algorithm

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str = '', options) {
+  let str = result,
+      repTimes = options.repeatTimes || 1,
+      separator = options.separator || '+',
+      addition = options.addition || '',
+      addRepTimes = options.additionRepeatTimes || 1,
+      addSeparator = options.additionSeparator || '|';
 };
   
