080de6f70de068fb651db16d5e705c24f9d46075
SamheinDM <SamheinDM@yandex.ru>
2020-10-29 22:29:51 +0300

    feat: complete extended-repeater algorithm

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,11 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str = '', options) {
-  let str = result,
+  let string = str,
+      result = '',
       repTimes = options.repeatTimes || 1,
       separator = options.separator || '+',
       addition = options.addition || '',
       addRepTimes = options.additionRepeatTimes || 1,
-      addSeparator = options.additionSeparator || '|';
+      addSeparator = options.additionSeparator || '',
+      countRep = 1,
+      countAddRep = 0;
+
+  if (options.addition === false) addition = 'false';
+  if (options.addition === null) addition = 'null';
+
+
+  for (let i = 0; i < repTimes; i++) {
+    result += `${String(string)}`;
+    for (let j = 0; j < addRepTimes; j++) {
+      result += `${String(addition)}`;
+      countAddRep++;
+      if (addRepTimes === countAddRep) {
+        continue;
+      } else {
+        result += `${String(addSeparator)}`;
+      }
+    }
+    if (repTimes === countRep) {
+      continue;
+    } else {
+      result += `${String(separator)}`;
+      countRep++;
+    }
+    countAddRep = 0;
+  }
+
+  return result;
 };
-  
+
+
+// -9UXKEEt8AqIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDg1L467Kdqx29UXKEEt8AqIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pb1L467Kdqx29UXKEEt8AqIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pb1L467Kdqx29UXKEEt8AqIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pb
+// +9UXKEEt8AqIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDg1L467Kdqx29UXKEEt8AqIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDg1L467Kdqx29UXKEEt8AqIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDg1L467Kdqx29UXKEEt8AqIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDgU7L9D0f8pbIMqCarClDg
